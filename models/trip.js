2d4e9f9920ba8cc61eed7c8090d9e8bc14edfc96
Thomas Ricci <thomasricci11@gmail.com>
2023-06-11 20:40:03 +0200

    started syncing models with app

--- models/trip.js
+++ models/trip.js
@@ -1,13 +1,20 @@
-const { compact, pick } = require('lodash');
-const mongoose = require('mongoose');
-const uniqueValidatorPlugin = require('mongoose-unique-validator');
+const { compact, pick } = require("lodash");
+const mongoose = require("mongoose");
+const uniqueValidatorPlugin = require("mongoose-unique-validator");
 
-const config = require('../config');
-const { includeRequested } = require('../utils/api');
-const { apiIdPlugin, hrefPlugin, parsePlugin, relatedHrefPluginFactory, timestampsPlugin, transientPropertyPluginFactory } = require('../utils/models');
+const config = require("../config");
+const { includeRequested } = require("../utils/api");
+const {
+  apiIdPlugin,
+  hrefPlugin,
+  parsePlugin,
+  relatedHrefPluginFactory,
+  timestampsPlugin,
+  transientPropertyPluginFactory,
+} = require("../utils/models");
 
 const Schema = mongoose.Schema;
-const tripLogger = config.logger('trip');
+const tripLogger = config.logger("trip");
 
 const tripSchema = new Schema({
   title: {
@@ -16,46 +23,81 @@ const tripSchema = new Schema({
     unique: true,
     uniqueValidator: true,
     minlength: 3,
-    maxlength: 100
+    maxlength: 100,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+    default: null,
+  },
+  budget: {
+    type: Number,
+    default: 0,
+  },
+  totalDistance: Number,
+  transportMethod: {
+    transportType: {
+      type: String,
+      enum: ["van", "car", "plane"],
+      default: "car",
+    },
+    price: Number,
   },
   description: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 50000
-  }
+    maxlength: 50000,
+  },
 });
 
 tripSchema.plugin(apiIdPlugin);
 tripSchema.plugin(hrefPlugin);
 tripSchema.plugin(parsePlugin);
-tripSchema.plugin(relatedHrefPluginFactory('User', { logger: tripLogger }));
+tripSchema.plugin(relatedHrefPluginFactory("User", { logger: tripLogger }));
 tripSchema.plugin(timestampsPlugin);
-tripSchema.plugin(transientPropertyPluginFactory('placesCount'));
+tripSchema.plugin(transientPropertyPluginFactory("placesCount"));
 tripSchema.plugin(uniqueValidatorPlugin);
 
-tripSchema.methods.toJSON = function(options = {}) {
-
+tripSchema.methods.toJSON = function (options = {}) {
   const serialized = {
     id: this.apiId,
-    ...pick(this, 'href', 'title', 'description', 'placesCount', 'userId', 'userHref', 'createdAt', 'updatedAt')
+    ...pick(
+      this,
+      "href",
+      "title",
+      "description",
+      "placesCount",
+      "userId",
+      "userHref",
+      "createdAt",
+      "updatedAt"
+    ),
   };
 
-  if (includeRequested(options.req, 'user', options.includeContext)) {
-    serialized.user = this.user.toJSON({ req: options.req, includeContext: compact([ options.includeContext, 'user' ]).join('.') });
+  if (includeRequested(options.req, "user", options.includeContext)) {
+    serialized.user = this.user.toJSON({
+      req: options.req,
+      includeContext: compact([options.includeContext, "user"]).join("."),
+    });
   }
 
   return serialized;
 };
 
-tripSchema.statics.apiResource = '/api/trips';
-tripSchema.statics.editableProperties = [ 'title', 'description' ];
+tripSchema.statics.apiResource = "/api/trips";
+tripSchema.statics.editableProperties = ["title", "description"];
 
 // Cascade delete
-tripSchema.pre('remove', async function() {
-  const Place = mongoose.model('Place');
+tripSchema.pre("remove", async function () {
+  const Place = mongoose.model("Place");
   tripLogger.debug(`Deleting all places related to trip ${this.apiId}`);
   await Place.remove({ trip: this.id });
 });
 
-module.exports = mongoose.model('Trip', tripSchema);
+module.exports = mongoose.model("Trip", tripSchema);
