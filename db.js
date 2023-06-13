48e6587604d19e514c2c95b34138e449ef6b4811
Thomas Ricci <thomasricci11@gmail.com>
2023-06-13 15:14:47 +0200

    fix dbname for api

--- db.js
+++ db.js
@@ -1,31 +1,37 @@
-const mongoose = require('mongoose');
-const { readdirSync } = require('fs');
+const mongoose = require("mongoose");
+const { readdirSync } = require("fs");
 
-const config = require('./config');
-const dbLogger = config.logger('db');
+const config = require("./config");
+const dbLogger = config.logger("db");
 
 // Make sure all models are registered
-const modelsDir = config.path('models');
+const modelsDir = config.path("models");
 const modelFiles = readdirSync(modelsDir);
 for (const modelFile of modelFiles) {
-  require(config.path('models', modelFile));
+  require(config.path("models", modelFile));
 }
 
 // Log database queries in debug mode
-if (dbLogger.isLevelEnabled('debug')) {
-  mongoose.set('debug', (collection, method, ...args) => dbLogger.debug(`${collection}.${method}(${args.map(arg => JSON.stringify(arg)).join(', ')})`));
+if (dbLogger.isLevelEnabled("debug")) {
+  mongoose.set("debug", (collection, method, ...args) =>
+    dbLogger.debug(
+      `${collection}.${method}(${args
+        .map((arg) => JSON.stringify(arg))
+        .join(", ")})`
+    )
+  );
 }
 
 // Remove deprecation warnings
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useNewUrlParser', true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useNewUrlParser", true);
 
 /**
  * Connects to the database.
  */
 async function connect() {
-  await mongoose.connect(config.db, { dbName: 'demo-travel-log' });
+  await mongoose.connect(config.db, { dbName: config.dbName });
 }
 
 module.exports = { connect };
