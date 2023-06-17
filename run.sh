ded44c90a8aff868c424e78fc876ae479242143a
Thomas Ricci <thomasricci11@gmail.com>
2023-06-17 13:50:31 +0200

    removed coutnries json back to app + updated run command

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -p "27017:27017" --name mongo mongo:latest &> /dev/null
+docker run -d -p "27017:27017" --name mongo mongo:latest
 export DATABASE_URL=mongodb://localhost/travel-log-api
 export DATABASE_NAME=travel-log-api
 export SECRET=alskdhiazsdf234234
