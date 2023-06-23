774ffc24c7f06a7c56cc95bf3acd580d76eb9af9
Thomas Ricci <thomasricci11@gmail.com>
2023-06-23 18:38:42 +0200

    updated run.sh Still needs improvement to be run if docker does not exist

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -p "27017:27017" --name mongo mongo:latest
+docker run -d -p "27017:27017" -v "travel-log-mongo:/data/db" --restart unless-stopped --name mongo mongo:latest
 export DATABASE_URL=mongodb://localhost/travel-log-api
 export DATABASE_NAME=travel-log-api
 export SECRET=alskdhiazsdf234234
