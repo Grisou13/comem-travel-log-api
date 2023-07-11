bee4d55f1d529522c5ed947cb5d90473980aadcb
Thomas Ricci <thomasricci11@gmail.com>
2023-07-11 15:03:43 +0200

    add pahination headers

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const cors = require('cors');
-const express = require('express');
-const { connectLogger } = require('log4js');
+const cors = require("cors");
+const express = require("express");
+const { connectLogger } = require("log4js");
 
 // Load database first to make sure all models are registered
-const db = require('./db');
+const db = require("./db");
 
-const apiRouter = require('./api');
-const config = require('./config');
+const apiRouter = require("./api");
+const config = require("./config");
 
 const app = express();
-const expressLogger = config.logger('express');
+const expressLogger = config.logger("express");
 
 // Log HTTP requests
-app.use(connectLogger(expressLogger, { level: 'trace' }));
+app.use(connectLogger(expressLogger, { level: "trace" }));
 
 // Parse the body of JSON requests
 app.use(express.json());
 
 // Allow CORS (if enabled)
 if (config.cors) {
-  app.use(cors());
-  expressLogger.debug('CORS is enabled');
+  app.use(
+    cors({
+      exposedHeaders: [
+        "Pagination-Page",
+        "Pagination-Page-Size",
+        "Pagination-Total",
+        "Pagination-Filtered-Total",
+      ],
+    })
+  );
+  expressLogger.debug("CORS is enabled");
 } else {
-  expressLogger.debug('CORS is disabled');
+  expressLogger.debug("CORS is disabled");
 }
 
 // Plug in the API
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // Display the documentation if available
-app.use(express.static('docs'));
+app.use(express.static("docs"));
 
 // Redirect root requests to the API
-app.use('/', (req, res) => res.redirect('/api'));
+app.use("/", (req, res) => res.redirect("/api"));
 
 /**
  * Starts the application by connecting to the database.
